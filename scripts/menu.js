0ed95a1d2748fe07b8c0c3c6b58492b208f1bbfe
alexymaria <luxdelux95@gmail.com>
2025-01-12 18:26:27 +0100

    push menu

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,8 +1,8 @@
-// Selecciona el botón del menú hamburguesa y los enlaces del menú
-const hamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".nav-links");
+document.addEventListener("DOMContentLoaded", function () {
+    const toggleButton = document.querySelector(".menu-toggle");
+    const menu = document.querySelector("nav ul");
 
-// Añade un evento para mostrar/ocultar el menú al hacer clic
-hamburger.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
+    toggleButton.addEventListener("click", function () {
+        menu.classList.toggle("show");
+    });
 });
