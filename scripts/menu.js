570776c97d308943c4972f219656763af5fdc8b2
alexymaria <luxdelux95@gmail.com>
2025-01-15 15:49:23 +0100

    cxvbnm,

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,19 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
     const toggleButton = document.querySelector(".menu-toggle"); // Botón hamburguesa
-    const mobileMenu = document.querySelector("#mobile-menu"); // Menú móvil
-    const menuLinks = mobileMenu.querySelectorAll("a"); // Enlaces del menú móvil
+    const menu = document.querySelector("#mobile-menu"); // Menú
+    const menuLinks = menu.querySelectorAll("a"); // Enlaces del menú
 
     // Alternar el menú al hacer clic en el botón
     toggleButton.addEventListener("click", function () {
         console.log("Toggle button clicked"); // Verificar el clic
-        mobileMenu.classList.toggle("show"); // Añade/quita la clase "show"
+        menu.classList.toggle("show"); // Añade/quita la clase "show"
     });
 
     // Cerrar el menú al hacer clic en un enlace
     menuLinks.forEach(link => {
         link.addEventListener("click", function () {
             console.log("Menu link clicked"); // Verificar el clic
-            mobileMenu.classList.remove("show"); // Asegurarse de que el menú se cierre
+            menu.classList.remove("show"); // Asegurarse de que el menú se cierre
         });
     });
 });
+
+
