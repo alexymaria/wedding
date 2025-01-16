6acf7780bbc1de79c0b8927b39ddcf645966afbb
alexymaria <luxdelux95@gmail.com>
2025-01-16 16:01:47 +0100

    cambiamos

--- scripts/preloadaer.js
+++ scripts/preloadaer.js
@@ -2,5 +2,5 @@ window.addEventListener("load", function () {
     const preloader = document.getElementById("preloader");
     setTimeout(() => {
         preloader.classList.add("hidden"); // Añade la clase para desvanecerlo
-    }, 1000); // Espera 1 segundo antes de ocultarlo
+    }, 1500); // Desaparece tras 1.5 segundos
 });
