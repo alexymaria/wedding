87ab9e63717fd619670a4fac13facbad759d4412
alexymaria <luxdelux95@gmail.com>
2025-01-17 10:20:46 +0100

    prueba footer + arreglo hamburguer?

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,21 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
     const toggleButton = document.querySelector(".menu-toggle"); // Botón hamburguesa
-    const menu = document.querySelector("#mobile-menu"); // Menú
+    const menu = document.querySelector("#mobile-menu"); // Menú móvil
     const menuLinks = menu.querySelectorAll("a"); // Enlaces del menú
 
     // Alternar el menú al hacer clic en el botón
     toggleButton.addEventListener("click", function () {
         console.log("Toggle button clicked"); // Verificar el clic
         menu.classList.toggle("show"); // Añade/quita la clase "show"
+        menu.classList.toggle("hidden"); // Quita/añade la clase "hidden"
     });
 
     // Cerrar el menú al hacer clic en un enlace
     menuLinks.forEach(link => {
         link.addEventListener("click", function () {
             console.log("Menu link clicked"); // Verificar el clic
-            menu.classList.remove("show"); // Asegurarse de que el menú se cierre
+            menu.classList.remove("show"); // Quitar la clase "show"
+            menu.classList.add("hidden"); // Añadir la clase "hidden"
         });
     });
 });
-
-
