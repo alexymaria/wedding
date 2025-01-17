0772051e5c39be07deb5baa390502fda736a0c94
alexymaria <luxdelux95@gmail.com>
2025-01-17 21:34:58 +0100

    update

---
+++ scripts/firebase.js
@@ -0,0 +1,22 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyACJzTzUT9fZdvoyGNFZkauJvIfJebSOik",
+    authDomain: "wedd-88c89.firebaseapp.com",
+    databaseURL: "https://wedd-88c89-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "wedd-88c89",
+    storageBucket: "wedd-88c89.firebasestorage.app",
+    messagingSenderId: "387760003871",
+    appId: "1:387760003871:web:07155124a2a69c5a7f6e2a",
+    measurementId: "G-C2CFYLZ973"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
