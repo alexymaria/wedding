31f9fb354efaa3e0b5a5886d6a07b699ff8a7812
alexymaria <luxdelux95@gmail.com>
2025-01-19 12:51:22 +0100

    arreglo gotoform

--- scripts/botontorsvp.js
+++ scripts/botontorsvp.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const goToFormButton = document.getElementById("goToFormButton");
 
     goToFormButton.addEventListener("click", function () {
-        const formSection = document.getElementById("form"); // Reemplaza con el ID del formulario
+        const formSection = document.getElementById("dataForm"); // Reemplaza con el ID del formulario
         if (formSection) {
             formSection.scrollIntoView({
                 behavior: "smooth", // Desplazamiento suave
