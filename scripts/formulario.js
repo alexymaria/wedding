1e316262de74500dc797b30176adc1e6abfc686c
alexymaria <luxdelux95@gmail.com>
2025-01-23 17:09:02 +0100

    quito analytics

--- scripts/formulario.js
+++ scripts/formulario.js
@@ -13,7 +13,7 @@ const firebaseConfig = {
   measurementId: "G-C2CFYLZ973"
 };
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+//const analytics = getAnalytics(app);
 const db = getFirestore(app);
 
 const form = document.getElementById('dataForm');
