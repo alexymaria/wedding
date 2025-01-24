26b08b747ea6a76b377206d65b3937e3f8a3544a
alexymaria <luxdelux95@gmail.com>
2025-01-24 12:02:02 +0100

    update

--- scripts/formulario.js
+++ scripts/formulario.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
 import { getFirestore, collection, addDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-analytics.js";
+import { getAnalytics, logEvent } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-analytics.js";
 
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -165,9 +165,6 @@ attendanceRadios.forEach(radio => radio.addEventListener('change', resetValidati
 [numGuestsInput].forEach(field =>
   field.addEventListener('input', resetValidation)
 );
-
-
-import { logEvent } from "firebase/analytics";
 
 
 document.getElementById("goToFormButton").addEventListener("click", () => {
